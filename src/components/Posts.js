c3ffebfcb9549197cd4132fd9232604fbb92d9ac
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-10 19:41:44 +0700

    rewrite data acquisition in async/await && reinstall dependencies

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,17 +1,25 @@
 import { useEffect, useState } from 'react';
 import Post from './Post';
 
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+
 export default function Posts() {
     const [ posts, setPosts ] = useState([]);
     const [ error, setError ] = useState('');
     const [ isLoading, setIsLoading ] = useState(true);
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(posts => setPosts(posts))
-        .catch((error) => setError(error.message))
-        .finally(() => setIsLoading(false));
+        (async function () {
+            try {
+                const response = await fetch(API_URL);
+                const posts = await response.json();
+                setPosts(posts);
+            } catch (error) {
+                setError(error.message);
+            }
+
+            setIsLoading(false);
+        })();
     }, []);
 
     if (error) return <h1>Error: {error}</h1>;
