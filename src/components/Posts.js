bf846e49d2511cc4cae158ee13a9a61e3496b701
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-10 18:09:27 +0700

    add data loading indication

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,13 +4,17 @@ import Post from './Post';
 export default function Posts() {
     const [ posts, setPosts ] = useState([]);
     const [ error, setError ] = useState('');
+    const [ isLoading, setIsLoading ] = useState(true);
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response => response.json())
         .then(posts => setPosts(posts))
-        .catch((error) => setError(error.message));
+        .catch((error) => setError(error.message))
+        .finally(() => setIsLoading(false));
     }, []);
+
+    if (isLoading) return <h1>Loading...🔃</h1>;
 
     if (error) return <h1>Error: {error}</h1>;
 
