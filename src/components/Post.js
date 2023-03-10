0235ed24568fb0653318d17dc337701415e4baa5
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-10 17:50:56 +0700

    add post styles

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,9 @@
+import './Post.css';
+
 export default function Post(props) {
     const { id, userId, title, body } = props;
     return (
-        <div>
+        <div className="post">
             <span>{id}</span>
             <h1>{title}</h1>
             <small>User ID: {userId}</small>
