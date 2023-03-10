5541322af9d05cc8565deabee90edd10a8d612b6
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-10 18:20:19 +0700

    add posts heading and refactor Posts component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -14,15 +14,14 @@ export default function Posts() {
         .finally(() => setIsLoading(false));
     }, []);
 
-    if (isLoading) return <h1>Loading...🔃</h1>;
-
     if (error) return <h1>Error: {error}</h1>;
 
     return (
-        <div>
-            {posts.map((post) => {
-                return <Post key={post.id} {...post}/>;
-            })}
-        </div>
+        <>
+            <h1>Posts</h1>
+            <hr/>
+            {isLoading ? (<h1>Loading...🔃</h1>) :
+                (posts.map((post) => <Post key={post.id} {...post}/>))}
+        </>
     );
 }
